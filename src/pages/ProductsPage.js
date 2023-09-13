b86ac69ca2cb35086c5b4c3deab9632f793d782b
Will <wilsonarizza@gmail.com>
2023-09-13 13:56:37 -0300

    Almost there with the interface

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -23,27 +23,23 @@ export default function ProductsPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard: Products | Minimal UI </title>
+        <title> Atletas </title>
       </Helmet>
 
       <Container>
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Products
+        Atletas
         </Typography>
 
         <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <ProductFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
-            <ProductSort />
+
+            <></>
           </Stack>
         </Stack>
 
         <ProductList products={PRODUCTS} />
-        <ProductCartWidget />
+        
       </Container>
     </>
   );
